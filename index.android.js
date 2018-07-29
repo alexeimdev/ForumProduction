34132398bcac9b0793c8b94a5e061e74555c97e3
Alexei Melikov <ALEXEIME@cellcom.co.il>
2018-07-29 16:30:53 +0300

    First commit

---
+++ index.android.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    View,
+    ScrollView,
+    Button,
+    ToastAndroid,
+} from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+// Screens:
+import ForumProduction from './src/screens/ForumProduction';
+
+// Components:
+import Header from './src/components/Header';
+
+export default class MyApp extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+                {/* <Header /> */}
+                <Tabs />
+            </View>
+        )
+    }
+}
+
+export const Tabs = TabNavigator({
+    ForumProduction: {
+        screen: ForumProduction,
+        title: "Forum Production"
+    },
+    ForumProduction2: {
+        screen: ForumProduction,
+        title: "Forum Production 2"
+    },
+})
+
+/*
+class TabViewExample extends PureComponent {
+    state = {
+        index: 0,
+        routes: [
+            { key: 'ForumProduction', title: 'ForumProduction' }
+        ],
+    };
+    _handleIndexChange = index => this.setState({ index });
+    _renderHeader = props => <TabBar {...props} />;
+    _renderScene = SceneMap({
+        'ForumProduction': ForumProduction
+    });
+
+    _onRequestChangeTab = index => this.setState({ index });
+
+    render() {
+        return (
+            <TabViewAnimated
+                style={styles.container}
+                navigationState={this.state}
+                renderScene={this._renderScene}
+                renderHeader={this._renderHeader}
+                onIndexChange={this._handleIndexChange}
+                onRequestChangeTab={this._onRequestChangeTab}
+            />
+        );
+    }
+}
+*/
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
+
+AppRegistry.registerComponent('MyApp', () => MyApp);
---
+++ src/screens/ForumProduction.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    ScrollView,
+    Button,
+    ToastAndroid,
+} from 'react-native';
+
+const ForumProduction = () => {
+    const buttonPressed = (pMethod, pData) => {
+        getData(pMethod, pData)
+    }
+    const getData = (pMethod, pData) => {
+        console.log(`getData => pMethod: ${pMethod}, pData: ${pData}`)
+        //let baseUrl = 'http://10.6.12.115/QC_VersionStatus/Forum/forumProd.asmx'
+        let baseUrl = 'http://mis0078213.corp.cellcom.co.il/Forum/forumProd.asmx'
+        let fullUrl = baseUrl + '/' + pMethod
+        let reqMethod = 'POST'
+
+        let reqHeaders = {
+            'Content-Type': 'application/json; charset=utf-8',
+            //'Access-Control-Allow-Origin': '*',
+            //'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+        }
+
+        let reqData = pData
+
+        fetch(fullUrl, {
+            method: reqMethod,
+            headers: reqHeaders,
+            data: reqData,
+        })
+            .then(response => response.json())
+            .then(response => {
+                if (response.d) {
+                    showShortToast(response.d)
+                }
+            })
+            .catch(error => showShortToast('error'))
+    }
+    const showShortToast = (text) => {
+        ToastAndroid.showWithGravity(text, ToastAndroid.SHORT, ToastAndroid.CENTER);
+    }
+
+    return (
+        <ScrollView style={styles.tab1}>
+            <View>
+                <Button title="GetForumForMe" onPress={() => buttonPressed('GetForumForMe', {})} />
+                <Button title="GetApprovedRequestsToday" onPress={() => buttonPressed('GetApprovedRequestsToday', {})} />
+                <Button title="GetDoneForumForMe" onPress={() => buttonPressed('GetDoneForumForMe', {})} />
+                <Button title="GetRequestsToday" onPress={() => buttonPressed('GetRequestsToday', {})} />
+                <Button title="HelloWorld" onPress={() => buttonPressed('HelloWorld', {})} />
+                <Button title="getAllRole" onPress={() => buttonPressed('getAllRole', { requestID: '93209' })} />
+                <Button title="getName" onPress={() => buttonPressed('getName', {})} />
+                <Button title="getRole" onPress={() => buttonPressed('getRole', { requestID: '93209' })} />
+                <Button title="testgetName" onPress={() => buttonPressed('testgetName', {})} />
+            </View>
+        </ScrollView >
+    )
+}
+
+const styles = StyleSheet.create({
+    tab1: {
+        backgroundColor: '#ff0000',
+    },
+    btn: {
+    }
+});
+
+export default ForumProduction
