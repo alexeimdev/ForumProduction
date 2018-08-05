7176962a8966022dc4ed6721b95a8edd7ca3ed69
alexeimdev <alexeim.dev@gmail.com>
2018-08-05 20:29:35 +0300

    added RTL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { I18nManager } from 'react-native';
 import TreatedStackNavigator from './navigation/TreatedStackNavigator';
 
 // Components:
 import Header from './components/Header';
 
+
 export default class MyApp extends Component {
     constructor(props) {
         super(props);
+        I18nManager.forceRTL(true);
     }
 
     render() {
